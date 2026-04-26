89a59bf3ef45982db062aa9699179edc713c6758
felixruntime <felixruntime.lab@gmail.com>
2026-04-26 15:00:49 +0000

    feat: new echo route

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def health():
     return(jsonify(status = "ok", service = "sinapse", version = "1.0.0"))
 
+@app.route("/echo/<text>")
+def echo(text):
+    return (jsonify(echo=text))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
