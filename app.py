c39492746b7e2ad1f5893d7451afc074fe0a01ab
felixruntime <felixruntime.lab@gmail.com>
2026-04-26 14:55:01 +0000

    feat: First app routes

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message": "Sinapse API Online!", "status": "success"}
+
+@app.route("/health")
+def health():
+    return(jsonify(status = "ok", service = "sinapse", version = "1.0.0"))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
